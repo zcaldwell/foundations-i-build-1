174ae851e92aa840f30475836b68ef4c19735216
zcaldwell <mintyst.john@gmail.com>
2021-10-08 15:05:27 -0700

    Well i found out the reason it wasn't working was a typo in my html, im resubmitting not for points but for my self respect

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // import functions and grab DOM elements
-const numberButton = document.getElementById('generate');
-const randomNumber = document.getElementById('random-num');
+const button = document.getElementById('generate');
+const randomNum = document.getElementById('random-num');
 // initialize global state
-Math.floor(Math.random() * 100);
+
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
-numberButton.addEventListener('click', () => {
-    const randomNumber = 
+
+button.addEventListener('click', ()=> {
+    var theRandomNum = Math.floor(Math.random() * 100) + 1 ;
+    console.log(theRandomNum);
+    randomNum.textContent = theRandomNum;
     
 });
