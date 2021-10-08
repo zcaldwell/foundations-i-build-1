5358cdc2336974f7d40305dfa9c4d0d17b99b18f
zcaldwell <mintyst.john@gmail.com>
2021-10-08 14:06:20 -0700

    .

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // import functions and grab DOM elements
-
+const numberButton = document.getElementById('generate');
+const randomNumber = document.getElementById('random-num');
 // initialize global state
-
-// set event listeners 
+Math.floor(Math.random() * 100);
 // get user input
 // use user input to update state 
 // update DOM to reflect the new state
-
+numberButton.addEventListener('click', () => {
+    const randomNumber = 
+    
+});
